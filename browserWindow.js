c1153c1768c3bb2e8d881ef6f5c843bc627493b2
Simon Jensen <Simonmarqvard@gmail.com>
2019-03-01 09:34:17 -0500

    first commit

---
+++ browserWindow.js
@@ -0,0 +1,28 @@
+const {
+  BrowserWindow
+} = require('electron')
+
+//hvad gør denne?
+// exports.win
+
+exports.createWindow = () => {
+  // Create the browser window.
+  this.win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // Open the DevTools.
+  this.win.webContents.openDevTools()
+
+  // and load the index.html of the app.
+  this.win.loadURL(`file://${__dirname}/renderer/index.html`)
+
+  // Emitted when the window is closed.
+  this.win.on('closed', () => {
+    this.win = null
+  })
+}
